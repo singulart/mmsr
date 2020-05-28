1dcf02bef7e5ca944fe1f0d409edaeffbd0179b8
Singulart <serpian@gmail.com>
2020-05-28 23:28:10 +0300

    - psnr

--- codes/data_scripts/metrics.py
+++ codes/data_scripts/metrics.py
@@ -1,8 +1,8 @@
 import re
 import sys
 
-metric = '([0-9]\.[0-9]+e[\-\+][0-9]+)'
-iter_number = r'iter: ([0-9]+,[0-9]+)'
+metric = '((-)*[0-9]\.[0-9]+e[\-\+][0-9]+)'
+iter_number = r'iter:[ ]*([0-9]+,[0-9]+)'
 l_g_pix = ''.join([r'l_g_pix: ', metric])
 l_g_fea = ''.join([r'l_g_fea: ', metric])
 l_g_gan = ''.join([r'l_g_gan: ', metric])
@@ -10,6 +10,8 @@ l_d_real = ''.join([r'l_d_real: ', metric])
 l_d_fake = ''.join([r'l_d_fake: ', metric])
 D_real = ''.join([r'D_real: ', metric])
 D_fake = ''.join([r'D_fake: ', metric])
+psnr = ''.join([r'PSNR: ', metric])
+
 
 with open(sys.argv[1]) as log:
     csv = open('metrics.csv', 'w')
@@ -42,3 +44,10 @@ with open(sys.argv[1]) as log:
                             g.group(1)]))
         csv.write('\n')
     csv.close()
+
+    with open('psnr.csv', 'w') as psnr_log:
+        psnr_log.write('PSNR')
+        psnr_log.write('\n')
+        for p in re.finditer(psnr, content):
+            psnr_log.write(p.group(1))
+            psnr_log.write('\n')
